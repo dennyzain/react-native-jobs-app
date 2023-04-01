290158908133ad14287aabcf70e4b9a480a336fd
Denny Abbas Zain <abbasdenny24@gmail.com>
2023-04-01 20:19:34 +0700

    Initial setup

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { useState } from "react";
+import { useRouter, Stack } from "expo-router";
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <View>
+      <Text>Hello world! Ws</Text>
+    </View>
+  );
+};
+
+export default Home;
