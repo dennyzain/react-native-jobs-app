3c410eaf0a10976d31dd6e2248f4fc2cc9d2812b
dennyzain <abbasdenny24@gmail.com>
2023-06-15 10:45:07 +0700

    fix: title nearby component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -14,7 +14,7 @@ const Welcome = () => {
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Bocah CBL</Text>
+        <Text style={styles.userName}>Hello Blog</Text>
         <Text style={styles.welcomeMessage}>Find Your Job</Text>
       </View>
       <View style={styles.searchContainer}>
