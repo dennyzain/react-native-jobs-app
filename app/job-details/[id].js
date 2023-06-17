9fff89f7a4cdfc5d99ece971bc0f140ea2ff5716
dennyzain <abbasdenny24@gmail.com>
2023-06-17 15:37:51 +0700

    feat: job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,10 +1,28 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
+import { COLORS, icons, images } from "../../constants";
+import { ScreenHeaderBtn } from "../../components";
+import { Stack, useRouter } from "expo-router";
 
 export default function JobDetail() {
+  const router = useRouter();
   return (
-    <View>
-      <Text>TEST</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: COLORS.lightWhite,
+          headerShadowVisible: false,
+          headerTitle: () => "",
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.menu}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />,
+        }}
+      />
+    </SafeAreaView>
   );
 }
